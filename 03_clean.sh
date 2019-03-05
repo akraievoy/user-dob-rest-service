3b3e4ee2172f93a5d285b3431b1e4456ee7cfb02
akraievoy <akraievoy@gmail.com>
2019-03-05 10:58:54 +0000

    now getting rid of all dangling blobs in git history

--- 03_clean.sh
+++ 03_clean.sh
@@ -3,7 +3,7 @@
 sudo chown -R "${whoami}:${whoami}" .
 chmod -R 770 .idea go_pkg_mod vendor
 rm -rf .idea go_pkg_mod vendor ingestor/ingestor upserter/upserter
-git gc
+git -c gc.reflogExpire=0 -c gc.reflogExpireUnreachable=0 -c gc.rerereresolved=0 -c gc.rerereunresolved=0 -c gc.pruneExpire=now gc
 docker-compose rm -f
 docker images | grep '<none>' | sed -E 's/\s+/\t/g' | cut -f 3 | xargs -r docker rmi -f
 docker system prune --volumes --force
